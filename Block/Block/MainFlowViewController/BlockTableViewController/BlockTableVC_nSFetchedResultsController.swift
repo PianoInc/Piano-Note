8533ecfb267e6e009152dc200969397f13055965
JangDoRi <JangDoRi@JangDoRis-MacBook-Pro.local>
2018-07-25 20:38:32 +0900

    D&D 거부하는거 수정

--- Block/Block/MainFlowViewController/BlockTableViewController/BlockTableVC_nSFetchedResultsController.swift
+++ Block/Block/MainFlowViewController/BlockTableViewController/BlockTableVC_nSFetchedResultsController.swift
@@ -31,55 +31,56 @@ extension BlockTableViewController: NSFetchedResultsControllerDelegate {
     }
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-            switch type {
-            case .delete:
-                guard let indexPath = indexPath else { return }
-                self.tableView.deleteRows(at: [indexPath], with: .none)
-                refreshOrderedTextIfNeeded(controller: controller, currentIndexPath: indexPath)
+        switch type {
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            self.tableView.deleteRows(at: [indexPath], with: .none)
+            refreshOrderedTextIfNeeded(controller: controller, currentIndexPath: indexPath)
+            
+        case .insert:
+            UIView.performWithoutAnimation {
+                guard var newIndexPath = newIndexPath else { return }
+                self.tableView.insertRows(at: [newIndexPath], with: .none)
                 
-            case .insert:
-                UIView.performWithoutAnimation {
-                    guard var newIndexPath = newIndexPath else { return }
-                    self.tableView.insertRows(at: [newIndexPath], with: .none)
-                    
-                    //issue: 타이밍 이슈 때문에 여기서 숫자를 갱신해줘야함
-                    guard let block = controller.object(at: newIndexPath) as? Block,
-                        let orderedTextBlock = block.orderedTextBlock,
-                        let count = controller.sections?.first?.numberOfObjects else { return }
-                    var num = orderedTextBlock.num
-                    while true {
-                        newIndexPath.row += 1
-                        num += 1
-                        guard newIndexPath.row < count,
-                            let nextBlock = controller.object(at: newIndexPath) as? Block,
-                            let nextOrderedTextBlock = nextBlock.orderedTextBlock else { return }
-                        nextOrderedTextBlock.num = num
-                        nextBlock.modifiedDate = Date() //UI 갱신용
-                    }
+                //issue: 타이밍 이슈 때문에 여기서 숫자를 갱신해줘야함
+                guard let block = controller.object(at: newIndexPath) as? Block,
+                    let orderedTextBlock = block.orderedTextBlock,
+                    let count = controller.sections?.first?.numberOfObjects else { return }
+                var num = orderedTextBlock.num
+                while true {
+                    newIndexPath.row += 1
+                    num += 1
+                    guard newIndexPath.row < count,
+                        let nextBlock = controller.object(at: newIndexPath) as? Block,
+                        let nextOrderedTextBlock = nextBlock.orderedTextBlock else { return }
+                    nextOrderedTextBlock.num = num
+                    nextBlock.modifiedDate = Date() //UI 갱신용
                 }
+            }
+            
+        case .update:
+            UIView.performWithoutAnimation {
+                guard let indexPath = indexPath,
+                    let block = controller.object(at: indexPath) as? Block,
+                    let cell = tableView.cellForRow(at: indexPath) as? TextBlockTableViewCell else { return }
                 
-            case .update:
-                UIView.performWithoutAnimation {
-                    guard let indexPath = indexPath,
-                        let block = controller.object(at: indexPath) as? Block,
-                        let cell = tableView.cellForRow(at: indexPath) as? TextBlockTableViewCell else { return }
-                    
-                    tableView.performBatchUpdates({
-                        cell.data = block
-                    }, completion: nil)
-                    
-                    
-                    //issue: 타이밍 이슈 때문에 여기서 숫자를 갱신해줘야함
-                    guard let orderedTextBlock = block.orderedTextBlock,
-                        let resultsController = controller as? NSFetchedResultsController<Block> else { return }
-                    block.adjustAfterOrder(currentNum: orderedTextBlock.num, resultsController: resultsController)
-                }
-
-            case .move:
-                guard let indexPath = indexPath, let newIndexPath = newIndexPath else { return }
-                self.tableView.moveRow(at: indexPath, to: newIndexPath)
+                tableView.performBatchUpdates({
+                    cell.data = block
+                }, completion: nil)
+                
+                
+                //issue: 타이밍 이슈 때문에 여기서 숫자를 갱신해줘야함
+                guard let orderedTextBlock = block.orderedTextBlock,
+                    let resultsController = controller as? NSFetchedResultsController<Block> else { return }
+                block.adjustAfterOrder(currentNum: orderedTextBlock.num, resultsController: resultsController)
+            }
+            
+        case .move:
+            UIView.performWithoutAnimation {
+                guard let indexPaths = tableView.indexPathsForVisibleRows else {return}
+                tableView.reloadRows(at: indexPaths, with: .none)
+            }
         }
-        
     }
     
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
