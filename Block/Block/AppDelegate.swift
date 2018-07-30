93b024f9fefb67d5942a27180ab5bd531e47a40e
hoemoon <hoemoon.song@gmail.com>
2018-07-30 18:42:11 +0900

    Add RestorationID to storyboard

--- Block/Block/AppDelegate.swift
+++ Block/Block/AppDelegate.swift
@@ -14,6 +14,14 @@ import Facebook
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
