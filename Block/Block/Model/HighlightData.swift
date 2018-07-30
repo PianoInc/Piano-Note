2333742348a2aeae2fc6872791309626cb92c05b
Kevin <Kevin_baba@naver.com>
2018-07-30 14:48:23 +0900

    update model info for detector and highlight

---
+++ Block/Block/Model/HighlightData.swift
@@ -0,0 +1,13 @@
+//
+//  PianoData.swift
+//  Block
+//
+//  Created by Kevin Kim on 2018. 7. 30..
+//  Copyright © 2018년 Piano. All rights reserved.
+//
+
+import Foundation
+
+struct Highlight: Codable {
+    let ranges: [NSRange]
+}
