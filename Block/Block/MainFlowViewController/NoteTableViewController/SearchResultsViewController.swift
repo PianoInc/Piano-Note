0c29b6a19d90c3ea6e6f41799d31834bfa2a59ab
hoemoon <hoemoon.song@gmail.com>
2018-08-02 15:26:05 +0900

    Change SearchResultsViewController from UITableViewController to UIViewController

---
+++ Block/Block/MainFlowViewController/NoteTableViewController/SearchResultsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultsViewController.swift
+//  Block
+//
+//  Created by hoemoon on 02/08/2018.
+//  Copyright © 2018 Piano. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+}
